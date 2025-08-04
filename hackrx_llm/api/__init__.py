0ab23e190aca1d637fb1fd2cab0ac77166ac79de
Aditya-Ranjan1234 <aditya87331@gmail.com>
2025-08-04 15:16:23 +0530

    Vercel Host

---
+++ hackrx_llm/api/__init__.py
@@ -0,0 +1,6 @@
+"""API package for HackRx LLM application."""
+
+# This file allows the api directory to be treated as a Python package
+
+# Import any modules that should be available when importing the package
+from .serverless import app  # noqa: F401
